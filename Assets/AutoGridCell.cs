ffc4948f6c19e1d86de2d1f6d6a6cb10303cbb26
startsai <simksong@gmail.com>
2014-10-15 14:53:45 +0800

    First commit (unity project)

---
+++ Assets/AutoGridCell.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class AutoGridCell : MonoBehaviour 
+{
+	GridLayoutGroup mGridLayoutGroup;
+	RectTransform mRectTransform;
+	void Awake()
+	{
+		mRectTransform = GetComponent<RectTransform>();
+		mGridLayoutGroup = GetComponent<GridLayoutGroup>();
+
+
+		Debug.Log(mRectTransform.parent.lossyScale.ToString());
+	}
+}
---
+++ Assets/Main.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Main : MonoBehaviour 
+{
+	Text m_TargetText;
+	public RectTransform m_DisplayPanelRTF;
+	public GameObject m_ItemListRes;
+
+	void Awake()
+	{
+		for(int i = 0;i<11;i++)
+		{
+			GameObject GOTS = GameObject.Instantiate(m_ItemListRes) as GameObject;
+			GOTS.transform.parent = m_DisplayPanelRTF.transform;
+		}
+	}
+
+	public void onClearInput()
+	{
+		if(m_TargetText!=null)
+			m_TargetText.text = "";
+	}
+	public void onInputMsg(string msg)
+	{
+		if(m_TargetText!=null)
+			m_TargetText.text += msg;
+	}
+
+}
